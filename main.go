b9717874c029653fee541590476315561d8dc56b
96234 <962349367@qq.com>
2022-08-06 19:55:56 +0800

    init

--- main.go
+++ main.go
@@ -9,9 +9,9 @@ import (
 // @version 1.0
 // @description mall
 // @termsofservice https://github.com/18211167516/Go-Gin-Api
-//// @securityDefinitions.apikey ApiKeyAuth
-//// @in header
-//// @name Authorization
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
 // @contact.name meme
 // @contact.email 962349367@qq.com
 // @host 127.0.0.1:8080
